918aff7cbef49c01bbfcd219ebd09a2931262965
mboullouz <mohamedboullouz@gmail.com>
2018-10-07 15:18:31 +0200

    experimental export defs 2

--- src/mboullouz-commons.d.ts
+++
@@ -1,7 +0,0 @@
-
-export { default as LinkedList } from "./lib/LinkedList";
-export { default as IObject } from "./lib/IObject";
-export { Logger } from "./lib/logging/Logger";
-export { Utils } from "./lib/utils/Utils";
-export { Http } from "./lib/Sf/Http";
-export { JSRouting, JSRoutingParams, SfConfig } from "./lib/Sf/SfConfig";
