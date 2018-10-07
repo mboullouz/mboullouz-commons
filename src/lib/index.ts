22cd64b732d5c7c1c21e42640008d96839a7a6ac
mboullouz <mohamedboullouz@gmail.com>
2018-10-07 15:22:00 +0200

    experimental export defs 3

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,4 +4,5 @@ export { default as IObject } from "./../lib/IObject";
 export { Logger } from "./../lib/logging/Logger";
 export { Utils } from "./../lib/utils/Utils";
 export { Http } from "./../lib/Sf/Http";
+export { UiHelper } from "./../lib/Ui/UiHelper";
 export { JSRouting, JSRoutingParams, SfConfig } from "./../lib/Sf/SfConfig";
